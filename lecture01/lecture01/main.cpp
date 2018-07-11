45bcba79e22210747b51fbe449ef36632eeadd9c
kida <tomo426.sakura1281m@gmail.com>
2018-07-11 20:20:33 +0900

    add sizeof func to string operator

--- lecture01/lecture01/main.cpp
+++ lecture01/lecture01/main.cpp
@@ -2,19 +2,19 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-double integral1(double a, double b, double n, double (*func)(double))
+double integral1(double a, double b, double n, double(*func)(double))
 {
 	double dx, sum, ans;
 	dx = (b - a) / n;
 	sum = 0.0;
-	for (int i = n-1; i > 0; i--) {
+	for (int i = n - 1; i > 0; i--) {
 		sum += func(a + (double)i*dx)*dx;
 	}
 
 	return sum;
 }
 
-double integral2(double a, double b, double n, double (*func)(double))
+double integral2(double a, double b, double n, double(*func)(double))
 {
 	double dx, sum, ans;
 	dx = (b - a) / n;
@@ -26,22 +26,37 @@ double integral2(double a, double b, double n, double (*func)(double))
 	return sum;
 }
 
-double fx(double x)
+double integral3(double a, double b, double n, double(*func)(double))
 {
-	return x*sin(x);
+	double dx, sum, ans;
+	dx = (b - a) / n;
+	sum = 0.0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += (func(a + (double)i*dx) + func(a + (double)(i + 1)*dx))*dx*0.5;
+	}
+	return sum;
 }
 
+double fx(double x)
+{
+	return x;// *sin(x);
+}
+int func(int m, int n, int *c) {}
 int main()
 {
 	double a, b, n;
+	int c[10][10];
 	double integ;
+	func(10, 10, c);
 	a = 0.0;
-	b = M_PI;
+	b = 10;// M_PI;
 	n = 16384;
 	printf("a: %lf b: %lf n: %lf\n", a, b, n);
 
-	printf("integral1: %lf\n", integral1(a, b, n, fx));
-	printf("integral2: %lf\n", integral2(a, b, n, fx));
+	printf("integral1: %.40lf\n", integral1(a, b, n, fx));
+	printf("integral2: %.40lf\n", integral2(a, b, n, fx));
+	printf("integral3: %.40lf\n", integral3(a, b, n, fx));
 
 	return 0;
 }
--- string_operation/string_operation/main.cpp
+++ string_operation/string_operation/main.cpp
@@ -87,12 +87,12 @@ int operate_array()
 		// operate array
 		// copy i2 to i1 specified num of word
 		// disable to overlap between words
-		memcpy(i1, i2, sizeof(int) * 2);
+		//memcpy(i1, i2, sizeof(int) * 2);
 
 		// operate array
 		// copy i2 to i2 specified num of word
 		// enable to overlap between words
-		memmove(&i2[0], &i2[1], sizeof(int) * 5);
+		//memmove(&i2[0], &i2[1], sizeof(int) * 5);
 	}
 
 	/* COMPARE FUNCTIONS */
@@ -101,7 +101,7 @@ int operate_array()
 		// return lager than   0 if i2 >  i1
 		// return              0 if i2 == i1
 		// return smaller than 0 if i2 <  i1
-		ret = memcmp(i2, i1, 1);
+		ret = memcmp(i2, i1, sizeof(int) * 4);
 	}
 
 	// set data to array specified num from to
@@ -113,18 +113,18 @@ int operate_array()
 int main()
 {
 	char *a = "bob 4 men";
-	char b[N*3] = "1,2,3,4,5,6,7,8,9,\n1,";
+	char b[N * 3] = "1,2,3,4,5,6,7,8,9,\n1,";
 	char name[N];
 	int  age;
 	char sex[N];
 	//operate_array();
-	//oeprate_words();
+	oeprate_words();
 	//sscanf(a, "%s %d %s", name, &age, sex);
-	a = strtok(b, ",");
-	for (int i = 0; i < 10; i++)
-	{
-		a = strtok(NULL, ",");
-	}
+	//a = strtok(b, ",");
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	a = strtok(NULL, ",");
+	//}
 
 	return 0;
 }
